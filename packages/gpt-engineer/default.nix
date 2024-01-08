9ef66002ad3deb330c2e639b0aed4a0966bc5653
DavHau <hsngrmpf+github@gmail.com>
2024-01-08 22:44:49 +0700

    add gpt-engineer

---
+++ packages/gpt-engineer/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  dream2nix,
+  lib,
+  ...
+}: let
+  gpt-engineer =
+    (lib.head (lib.attrValues config.groups.default.packages.gpt-engineer)).public;
+in {
+  imports = [
+    dream2nix.modules.dream2nix.WIP-python-pdm
+  ];
+
+  deps = {nixpkgs, ...}: {
+    python3 = nixpkgs.python311;
+  };
+
+  pdm.lockfile = ./pdm.lock;
+  pdm.pyproject = ./pyproject.toml;
+  pdm.pythonInterpreter = config.deps.python3;
+  mkDerivation = {
+    src = ./.;
+    buildInputs = [
+      config.deps.python3.pkgs.pdm-backend
+    ];
+    postFixup = ''
+      mkdir -p $out/bin
+      for bin in $(ls ${gpt-engineer}/bin/); do
+        ln -s ${gpt-engineer}/bin/$bin $out/bin/$bin
+      done
+    '';
+  };
+  overrides.gpt-engineer = {
+    mkDerivation.buildInputs = [
+      config.deps.python3.pkgs.poetry-core
+    ];
+    mkDerivation.propagatedBuildInputs = [
+      config.deps.python3.pkgs.tkinter
+    ];
+  };
+}
